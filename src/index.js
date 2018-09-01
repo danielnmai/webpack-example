4347577d8aeb254df0fe5a37365eb528492cce53
danielnmai <danielmai1728@gmail.com>
2018-09-01 09:16:42 -0700

    perform output management

--- src/index.js
+++ src/index.js
@@ -1,20 +1,27 @@
 import _ from "lodash"
 import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml'
+// import Icon from './icon.png'
+// import Data from './data.xml'
+import printMe from './print.js'
 
 function component(){
     let element = document.createElement('div')
+    let button = document.createElement('button')
+
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
 
-    //Add the image
-    let myIcon = new Image()
-    myIcon.src = Icon
-    element.appendChild(myIcon)
+    // //Add the image
+    // let myIcon = new Image()
+    // myIcon.src = Icon
+    // element.appendChild(myIcon)
+    // console.log(Data)
 
-    console.log(Data)
+    button.innerHTML = 'Click me!'
+    button.onclick = printMe
+
+    element.appendChild(button)
 
     return element
 }
